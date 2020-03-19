9bff6467fe254de697a00a0d0d026598407ac4b9
Dhiti Painter <dcpainter@cygnet.com>
2020-03-19 15:46:59 +0530

    added update functionality, integrated google maps and solved linting errors

--- src/app/modules/event/event-routing.module.ts
+++ src/app/modules/event/event-routing.module.ts
@@ -5,9 +5,9 @@ import { AddEventComponent } from './add-event/add-event.component';
 import { EventListComponent } from './event-list/event-list.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'add', pathMatch:'full'},
-  {path: 'add', component: AddEventComponent},
-  {path: 'list', component: EventListComponent},
+  { path: '', redirectTo: 'add', pathMatch: 'full' },
+  { path: 'add', component: AddEventComponent },
+  { path: 'list', component: EventListComponent },
 ];
 
 export const EventRoutingModule: ModuleWithProviders = RouterModule.forChild(routes);
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,28 +1,28 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatInputModule } from "@angular/material/input";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatButtonModule } from "@angular/material/button";
-import { MatTableModule } from "@angular/material/table";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatRadioModule } from "@angular/material/radio";
-import { MatIconModule } from "@angular/material/icon";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import {
   NgxMatDatetimePickerModule,
   NgxMatTimepickerModule,
   NgxMatNativeDateModule
-} from "ngx-mat-datetime-picker";
-import { MatNativeDateModule } from "@angular/material";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
+} from 'ngx-mat-datetime-picker';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
   imports: [
@@ -70,6 +70,11 @@ import { MatAutocompleteModule } from "@angular/material/autocomplete";
     NgxMatNativeDateModule,
     MatGridListModule,
     MatAutocompleteModule
+  ],
+  providers: [
+    {
+      provide: MAT_DATE_LOCALE, useValue: 'en-GB'
+    }
   ]
 })
-export class MaterialModule {}
+export class MaterialModule { }
